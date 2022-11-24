d8749bd357c523ac221e5db0fe30f02a9e830024
MaykonFranchini <maykonfranchini2017@gmail.com>
2022-11-24 11:40:22 +0000

    Proeject setup + global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding:0;
+  box-sizing: border-box;
+}
+
+body {
+  background: ${(props) => props.theme.background};
+  color: ${(props) => props.theme['gray-500']};
+
+  -webkit-font-smoothing: antialiased;
+
+}
+
+body, input, text-area, button {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+}
+
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+ cardBackground: '#F3F2F2',
+ background: '#FAFAFA',
+
+
+  // 'gray-100': '#E1E1E6',
+  // 'gray-300': '#C4C4CC',
+  // 'gray-400': '#8D8D99',
+  'gray-500': '#574F4D',
+  'gray-700': '#403937',
+  'gray-900': '#272221',
+
+  // 'green-300': '#00B37E',
+  // 'green-500': '#00875F',
+  // 'green-700': '#015F43',
+
+  // 'red-500': '#AB222E',
+  // 'red-700': '#7A1921',
+
+  // 'yellow-500': '#FBA94C',
+}
